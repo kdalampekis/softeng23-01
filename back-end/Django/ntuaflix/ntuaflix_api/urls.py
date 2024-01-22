fdc75ea715e08427094d556a5d13dc77d82f3dc5
ntua-el21406 <el21406@mail.ntua.gr>
2024-01-22 19:52:30 +0200

    modified:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/urls.cpython-312.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/views.cpython-312.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/templates/NBestRatedGenre.html 	new file:   back-end/Django/ntuaflix/ntuaflix_api/templates/SearchByGenre.html 	new file:   back-end/Django/ntuaflix/ntuaflix_api/templates/SearchByName.html 	new file:   back-end/Django/ntuaflix/ntuaflix_api/templates/SearchByYear.html 	modified:   back-end/Django/ntuaflix/ntuaflix_api/urls.py 	modified:   back-end/Django/ntuaflix/ntuaflix_api/views.py

--- back-end/Django/ntuaflix/ntuaflix_api/urls.py
+++ back-end/Django/ntuaflix/ntuaflix_api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import TitleBasicList,TitleDetailView
-from .views import SearchTitleView,FilteredTitleObjectsView,NameObjectView,NameObjectDetailView,SearchNameView
-
+from .views import *
 urlpatterns = [
     # ... other url patterns ...
 
@@ -10,23 +8,42 @@ urlpatterns = [
     path('title/', TitleBasicList.as_view(), name='title-basic-list'),
     path('title/<str:titleID>/', TitleDetailView.as_view(), name='title-detail'),
     path('searchtitle/', SearchTitleView.as_view(), name='search-title'),
+
+        # /////////////////     FILTERS     ////////////////////////////////////////
     path('bygenre/', FilteredTitleObjectsView.as_view(), name='filtered-title-objects'),
     
+
+        # /////////////////     NAMES     ////////////////////////////////////////
+
     path('name/', NameObjectView.as_view(), name='name-basic-list'),
-    path('name/<str:nameID>/', NameObjectDetailView.as_view(), name='name-detail'),
+    path('name/<str:nameID>/', NameBiography.as_view(), name='name-detail'),
     path('searchname/', SearchNameView.as_view(), name='search-name'),
 
-    # //////////////////////////////////////////////////////////////////
+    # /////////////////////////     END OF NECESSARY ENDPOINTS   /////////////////////////
     # //////////////////////////////////////////////////////////////////
 
-    # ///////////////////////////   END POINTS FROM REQUIREMENTS    ////////////////////
+
+
+    # ///////////////////////////   BELOW ARE ENDPOINTS FROM REQUIREMENTS    ////////////////////
 
     # 1ST REQUIREMENT:
     # Choose N movies with the best rating from a specific genre
+    path('NBestRatedGenre/', NBestRatedGenre.as_view(), name='NBestRatedGenre'),
 
 
-    path('1strequirement/')
+    # 4th Requirement    
+    # path('SearchByNameTopRated/', SearchByNameTopRated.as_view(), name='SearchByNameTopRated'),
 
+
+    # 16th Requirement    
+    path('SearchByGenre/', SearchByGenre.as_view(), name='SearchByGenre'),
+    
+    # 17th Requirement    
+    path('SearchByYear/', SearchByYear.as_view(), name='SearchByYear'),
+
+    
+    # 15th Requirement    
+    path('SearchByName/', SearchByName.as_view(), name='SearchByName'),
 
     # //////////////////////////////////////////////////////////////////////////////////
 
--- back-end/Django/ntuaflix/ntuaflix_api/views.py
+++ back-end/Django/ntuaflix/ntuaflix_api/views.py
@@ -4,7 +4,7 @@ from .serializers import TitleObjectSerializer,NameObjectSerializer
 from django.shortcuts import render
 from .forms import TitleSearchForm
 from rest_framework.views import APIView
-from rest_framework_csv.renderers import CSVRenderer
+# from rest_framework_csv.renderers import CSVRenderer
 from rest_framework.response import Response
 from django.db.models import Q
 
@@ -40,6 +40,8 @@ class SearchTitleView(APIView):
             # Render the search form if no query is provided
             return render(request, 'search_title.html')
 
+
+
 class FilteredTitleObjectsView(APIView):
 
     def get(self, request, *args, **kwargs):
@@ -71,6 +73,14 @@ class FilteredTitleObjectsView(APIView):
         # Return the response
         return Response(serializer.data)
 
+
+
+
+
+
+
+
+
 class NameObjectView(generics.ListAPIView):
     serializer_class = NameObjectSerializer
 
@@ -81,13 +91,15 @@ class NameObjectView(generics.ListAPIView):
 
         return queryset
    
-class NameObjectDetailView(generics.ListAPIView):
+class NameBiography(generics.ListAPIView):
     serializer_class = NameObjectSerializer
 
     def get_queryset(self):
 
         nameID = self.kwargs.get('nameID')
         return NameObject.objects.filter(nconst=nameID)
+
+
 
 class SearchNameView(APIView):
     def get_search_by_name(self, request):
@@ -99,3 +111,130 @@ class SearchNameView(APIView):
         else:
             # Render the search form if no query is provided
             return render(request, 'search_name.html')
+
+
+class NBestRatedGenre(APIView):
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve query parameters
+        genre = request.GET.get('genre', None)
+        numberofmovies = request.GET.get('numberofmovies', None)
+        
+        # Check if the 'genre' parameter is provided
+        if genre:
+            # Filter by genre
+            queryset = TitleObject.objects.filter(genres__icontains=genre)
+
+            # If 'numberofmovies' is provided, cast it to an integer and get the top N movies
+            if numberofmovies:
+                try:
+                    numberofmovies = int(numberofmovies)
+                    queryset = queryset.order_by('-averageRating')[:numberofmovies]
+                except ValueError:
+                    # Handle the exception if 'numberofmovies' is not a valid integer
+                    return Response({"error": "numberofmovies must be an integer"}, status=400)
+
+            # Serialize the queryset
+            serializer = TitleObjectSerializer(queryset, many=True)
+
+            # Return the serialized data
+        else:
+            # If 'genre' is not provided, render the search criteria form
+            return render(request, 'NBestRatedGenre.html')
+
+        return Response(serializer.data)
+
+
+
+class SearchByGenre(APIView):
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve query parameters
+        genre = request.GET.get('genre', None)        
+        # Check if the 'genre' parameter is provided
+        if genre:
+            # Filter by genre
+            queryset = TitleObject.objects.filter(genres__icontains=genre)
+            # Serialize the queryset
+            serializer = TitleObjectSerializer(queryset, many=True)
+
+            # Return the serialized data
+        else:
+            # If 'genre' is not provided, render the search criteria form
+            return render(request, 'SearchByGenre.html')
+
+        return Response(serializer.data)
+    
+
+
+
+class SearchByYear(APIView):
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve query parameters
+        year = request.GET.get('year', None)        
+        # Check if the 'genre' parameter is provided
+        if year:
+            # Filter by genre
+            queryset = TitleObject.objects.filter(startYear=year)
+            # Serialize the queryset
+            serializer = TitleObjectSerializer(queryset, many=True)
+
+            # Return the serialized data
+        else:
+            # If 'genre' is not provided, render the search criteria form
+            return render(request, 'SearchByYear.html')
+
+        return Response(serializer.data)
+    
+
+
+class SearchByName(APIView):
+
+    def get(self, request, *args, **kwargs):
+        # Retrieve query parameters
+        name = request.GET.get('name', None)        
+        toprated = request.GET.get('toprated',None)
+        newest = request.GET.get('newest',None)
+        number = request.GET.get('number',None)
+        # Check if the 'genre' parameter is provided
+        if name:
+            # Filter by genre
+            queryset = TitleObject.objects.filter(primaryName__icontains=name)
+            
+            if (toprated == 'true' and newest== 'true'):
+                context = {
+                'error_message': "You can only choose 'Earliest Release' or 'Top Rated', not both."
+                }
+                return render(request, 'SearchByName.html', context)
+
+            if number:
+                if toprated=='true':
+                    queryset = queryset.order_by('-averageRating')[:int(number)]
+                # Serialize the queryset
+                
+                if newest=='true':
+                    queryset = queryset.order_by('-startYear')[:int(number)]
+            else:
+                if toprated=='true':
+                    queryset = queryset.order_by('-averageRating')[:1]
+                # Serialize the queryset
+                
+                if newest=='true':
+                    queryset = queryset.order_by('-startYear')[:1]
+
+
+
+            serializer = TitleObjectSerializer(queryset, many=True)
+            # Return the serialized data
+        else:
+            # If 'genre' is not provided, render the search criteria form
+            return render(request, 'SearchByName.html')
+
+        return Response(serializer.data)
+
+
+
+
+
+
