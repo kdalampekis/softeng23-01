ee1acd48878c4672052b2e4b6eba63504befee1d
Stelios <stelioskatsis12@gmail.com>
2024-01-24 13:51:14 +0200

    new file:   back-end/Django/ntuaflix/ntuaflix/__pycache__/__init__.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix/__pycache__/settings.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix/__pycache__/urls.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix/__pycache__/wsgi.cpython-311.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix/settings.py 	modified:   back-end/Django/ntuaflix/ntuaflix/urls.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/__init__.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/admin.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/apps.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/forms.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/models.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/serializers.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/urls.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/views.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__init__.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/__init__.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/admin.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/apps.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/forms.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/models.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/urls.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/views.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/admin.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/apps.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/forms.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/migrations/__init__.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/migrations/__pycache__/__init__.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/models.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_titlebasics.html 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/tests.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/urls.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/administrator/views.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/migrations/0002_user.py 	new file:   back-end/Django/ntuaflix/ntuaflix_api/migrations/__pycache__/0001_initial.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/migrations/__pycache__/0002_user.cpython-311.pyc 	new file:   back-end/Django/ntuaflix/ntuaflix_api/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix_api/urls.py

--- back-end/Django/ntuaflix/ntuaflix_api/urls.py
+++ back-end/Django/ntuaflix/ntuaflix_api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+
 urlpatterns = [
-    # ... other url patterns ...
+    path('admin/', include('ntuaflix_api.administrator.urls')),
 
     # ///////////////////////////   END POINTS NECESSARY           ////////////////////////////
 
