ad9b6789b3f85008ca8fd168d5d7aceffb78336f
ntua-el21406 <el21406@mail.ntua.gr>
2024-01-25 17:47:48 +0200

    modified:   back-end/Django/ntuaflix/db.sqlite3 	modified:   back-end/Django/ntuaflix/ntuaflix_api/__pycache__/views.cpython-312.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/forms.cpython-312.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/urls.cpython-312.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix_api/administrator/__pycache__/views.cpython-312.pyc 	modified:   back-end/Django/ntuaflix/ntuaflix_api/administrator/forms.py 	deleted:    back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_crew.html 	deleted:    back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_principals.html 	deleted:    back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_rating.html 	deleted:    back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_titleaka.html 	deleted:    back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_titlebasics.html 	renamed:    back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/administrator/upload_names.html -> back-end/Django/ntuaflix/ntuaflix_api/administrator/templates/upload.html 	modified:   back-end/Django/ntuaflix/ntuaflix_api/administrator/urls.py 	modified:   back-end/Django/ntuaflix/ntuaflix_api/administrator/views.py

--- back-end/Django/ntuaflix/ntuaflix_api/administrator/forms.py
+++ back-end/Django/ntuaflix/ntuaflix_api/administrator/forms.py
@@ -1,22 +1,4 @@
 from django import forms
 
-class TitleBasicUploadForm(forms.Form):
+class BasicForm(forms.Form):
     tsv_file = forms.FileField(label='Upload TSV file')
-
-class UploadTitleAkasForm(forms.Form):
-    tsv_file = forms.FileField(label='Upload TSV file')
-
-class UploadNamesForm(forms.Form):
-    tsv_file = forms.FileField(label='Upload TSV file')
-
-class UploadRatingForm(forms.Form):
-    tsv_file = forms.FileField(label='Upload TSV file')
-
-class UploadPrincipalsForm(forms.Form):
-    tsv_file = forms.FileField(label='Upload TSV file')
-
-class UploadCrewForm(forms.Form):
-    tsv_file = forms.FileField(label='Upload TSV file')
-
-# class UploadPrincipalsForm(forms.Form):
-#     tsv_file = forms.FileField(label='Upload TSV file')
--- back-end/Django/ntuaflix/ntuaflix_api/administrator/urls.py
+++ back-end/Django/ntuaflix/ntuaflix_api/administrator/urls.py
@@ -4,11 +4,10 @@ from .views import *
 urlpatterns = [
     path('upload/titlebasics/', UploadTitleBasics, name='upload_title_basics'),
     path('upload/titleakas/', UploadTitleAkas, name='upload_title_akas'),
-    path('upload/names/', UploadNames, name='upload_names'),
-    path('upload/principals/', UploadPrincipals, name='upload_principals'),
-    path('upload/rating/', UploadRating, name='upload_rating'),
-
-    path('upload/crew/', UploadCrew, name='upload_crew'),
-
+    path('upload/namebasics/', UploadNameBasics, name='upload_name_basics'),
+    path('upload/titlecrew/', UploadTitleCrew, name='upload_title_crew'),
+    path('upload/titleepisode/', UploadTitleEpisode, name='upload_title_episode'),
+    path('upload/titleprincipals/', UploadTitlePrincipals, name='upload_title_principals'),
+    path('upload/titleratings/', UploadTitleRatings, name='upload_title_ratings'),
     path('healthcheck', health_check, name='health_check'),
 ]
