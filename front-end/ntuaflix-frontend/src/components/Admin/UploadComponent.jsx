5673f7d23dd8e4476f6b90fddd37aeb9892e6167
GeorgeSeretakos <g.seretakos@gmail.com>
2024-01-27 20:31:50 +0200

    Upload Component

--- front-end/ntuaflix-frontend/src/components/Admin/UploadComponent.jsx
+++ front-end/ntuaflix-frontend/src/components/Admin/UploadComponent.jsx
@@ -18,8 +18,23 @@ const UploadComponent = () => {
 
     // Handle file selection
     const handleFileChange = (event) => {
-        setFile(event.target.files[0]);
+        const fileInput = event.target;
+        const fileNameDisplay = document.getElementById('file-name');
+
+        // If a file is selected
+        if (fileInput.files.length > 0) {
+            const file = fileInput.files[0];
+            setFile(file); // Update the state with the selected file
+            const fileName = file.name;
+            fileNameDisplay.textContent = fileName; // Display the file name
+            label.classList.remove('no-file-selected'); // Remove the class indicating no file is selected
+        } else {
+            setFile(null); // Update the state to no file
+            fileNameDisplay.textContent = 'No file chosen...'; // Reset the display text
+            label.classList.add('no-file-selected'); // Add the class indicating no file is selected
+        }
     };
+
 
     // Handle file selection
     const handleUpload = async () => {
@@ -90,19 +105,30 @@ const UploadComponent = () => {
     };
 
 
+
     return (
         <div>
             <Header />
             <h1 className="message">Upload {message}</h1>
             {type && (
                 <div>
-                    <input type="file" onChange={handleFileChange} />
-                    <button onClick={handleUpload}>Upload</button>
+                    <div className="inputContainer fileInputContainer">
+                        <label htmlFor="file-upload" className="custom-file-label">
+                            <span>Choose file... </span>
+                            <span id="file-name"></span>
+                        </label>
+                        <input id="file-upload" type="file" onChange={handleFileChange}
+                               className="custom-file-input no-file-selected"/>
+
+                    </div>
+
+
+                    <div className="buttonContainer">
+                        <button onClick={handleUpload}>Upload</button>
+                        <button onClick={() => navigate(-1)}>Cancel</button>
+                    </div>
                 </div>
             )}
-            <div className="buttonContainer">
-                <button onClick={() => navigate(-1)}>Cancel</button>
-            </div>
             <Footer role="admin"/>
         </div>
     );
