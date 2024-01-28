71b800e90f51d308bd43c78caf242764c3cafcdb
GeorgeSeretakos <g.seretakos@gmail.com>
2024-01-28 22:04:02 +0200

    ResetAll.jsx

--- front-end/ntuaflix-frontend/src/components/Admin/ResetAll.jsx
+++ front-end/ntuaflix-frontend/src/components/Admin/ResetAll.jsx
@@ -1,30 +1,53 @@
 import Header from "../Header";
 import Footer from "../Footer";
-import React from "react";
+import React, {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import "../../styles.css";
+import axios from "axios";
+
+const BASE_URL = 'http://127.0.0.1:9876/ntuaflix_api/admin';
 
 
 export default function ResetAll() {
     const navigate = useNavigate();
+    const [resetDone, setResetDone] = useState(false);
 
     const handleReset = () => {
-        return null;
+        axios.post(`${BASE_URL}/resetall/`)
+            .then(response => {
+                // Handle success
+                console.log(response.data);
+                setResetDone(true);
+                alert("Reset successful!");
+                navigate(-1);
+            })
+            .catch(error => {
+                // Handle error
+                console.error("Reset failed:", error);
+                alert("Reset failed!");
+            });
     }
 
     return (
-        <div>
-            <Header/>
-            <h1 className="message">Are you sure you want to restore everything?</h1>
-            <h2>Your action can <span className="dynamic-content">not</span> be undone</h2>
-            {/* Render form or content based on the type */}
-            <div className="buttonContainer">
-                <button onClick={handleReset}>Reset</button>
-                <button onClick={() => navigate(-1)}>
-                    Cancel
-                </button>
+            <div>
+                <Header />
+
+                {!resetDone ? (
+                    <div>
+                        <h1 className="message">Are you sure you want to restore everything?</h1>
+                        <h2>Your action can <span className="dynamic-content">not</span> be undone</h2>
+                        <div className="buttonContainer">
+                            <button onClick={handleReset}>Reset</button>
+                            <button onClick={() => navigate(-1)}>Cancel</button>
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <h2>Reset complete!</h2>
+                    </div>
+                )}
+
+                <Footer role="admin"/>
             </div>
-            <Footer role="admin"/>
-        </div>
     );
 }
